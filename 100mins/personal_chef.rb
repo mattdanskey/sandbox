272a40713a758e17ebf3719438c826b7c5d04faf
eland <mdanskey@gmail.com>
2012-07-28 13:22:53 -0700

    ~1/3 done with Ruby in 100 mins

---
+++ 100mins/personal_chef.rb
@@ -0,0 +1,44 @@
+class PersonalChef
+  def make_waffles
+    puts "Making waffles!"
+  end
+  def make_donuts
+    puts "Making donuts!"
+  end
+end
+
+wilfred = PersonalChef.new
+wilfred.make_waffles
+wilfred.make_donuts
+
+class Butler # It seems class names are capitalized
+  def open_front_door # but methods are not capitalized.
+    puts "Opening the front door, sir."
+  end
+end
+
+sam = Butler.new
+sam.open_front_door
+
+class PersonalChef
+  def make_waffles(texture)
+    puts "Making #{texture} waffles!"
+  end
+end
+
+wilfred.make_waffles('fluffy')
+wilfred.make_waffles 'dense'
+
+=begin 
+wilfred.make_waffles no longer works. Looks like redefining a method
+totally replaces it.
+=end
+
+class PersonalChef
+  def make_donuts(flavor)
+    puts "Making #{flavor} donuts!"
+  end
+end
+
+wilfred.make_donuts('cake')
+
