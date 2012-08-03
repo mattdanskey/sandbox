8ce818c9d77e000e33947cfb6024042b768ab144
eland <mdanskey@gmail.com>
2012-08-03 16:44:38 -0700

    Now works with all valid roman numerals

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,39 +1,50 @@
-#This program parses roman numerals.
+#This method parses Roman numerals.
 
-def numeral numerals
-  numhash = {"I" => 1, "V" => 5, "X" => 10}
-  transposed = []
+def parse_numeral numerals
+  numhash = {
+	    "I" => 1, 
+	    "V" => 5, 
+	    "X" => 10,
+	    "L" => 50,
+	    "C" => 100,
+	    "D" => 500,
+	    "M" => 1000
+	    }
+  numarray = []
   
   numerals.each_char do |char|
-    transposed << numhash[char]
+    numarray << numhash[char]
   end
-  if transposed.length == 1
-    puts transposed[0]
+  if numarray.length == 1 #Nothing fancy needed with just one numeral
+    puts numarray[0]
   else
-    k = transposed.length
-    for i in 1..k
-      j = i-1
-      if transposed[-i] == transposed[-j]
-        transposed[-i] += transposed[-j]
-	transposed.delete_at(-j)
-      end
+    # Combine all similar adjacent numerals using regexes
+    numhash.each do |numer|
+	a = /#{numer}{2,}/ =~ numerals
+	unless a.nil?
+	    b = numerals.match /#{numer}{2,}/
+	    c = b[0].length #how many of the same numeral are in a row
+	    numarray[a] = numarray[a] * c
+	    numarray.slice!(a+1,c-1)
+	end
     end
-
-    while transposed.length > 1
-      if transposed[-1] > transposed[-2]
-        transposed[-2] = transposed [-1] - transposed[-2]
-        transposed.delete_at(-1)
+    
+    # Add together all values according to the rules of roman numerals
+    while numarray.length > 1
+      if numarray[-1] > numarray[-2] # ex. IV is evaluated 5 - 1
+        numarray[-2] = numarray [-1] - numarray[-2]
+        numarray.delete_at(-1)
       else
-        transposed[-2] += transposed[-1]
-	transposed.delete_at(-1)
+        numarray[-2] += numarray[-1]
+	numarray.delete_at(-1)
       end
     end
-    puts transposed[0]
+    puts numarray[0]
   end
 end
 
-numeral("XIV")
-numeral("III")
-numeral("V")
-numeral("IV")
-numeral("VI")
+parse_numeral("XIV")
+parse_numeral("III")
+parse_numeral("V")
+parse_numeral("IV")
+parse_numeral("VI")
