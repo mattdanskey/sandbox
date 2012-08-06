1e97b179647ac24e14b319628d93592d2aeb138b
eland <mdanskey@gmail.com>
2012-08-06 14:56:42 -0700

    First commit for following 'getting started' official guide and making a blog

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a95ced2afbf837cd6562fb94c9b170b469a85a7d5cfe8098ab42e44d1378dd9c9122a3c30015bfcde00a5cc356404c94707c59c541447cb115aaa013baaa634a'
