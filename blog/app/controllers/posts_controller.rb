5c0f7fa4b5447034b971c41855da8ad99d24658c
eland <mdanskey@gmail.com>
2012-08-07 11:17:37 -0700

    Can now edit (update) posts

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,20 +1,40 @@
 class PostsController < ApplicationController
 
   def new
-  
+    @post = Post.new
   end
   
   def create
     @post = Post.new(params[:post])
     
-    @post.save
-    redirect_to :action => :show, :id => @post.id
-    
+    if @post.save
+      redirect_to :action => :show, :id => @post.id
+    else
+      render 'new'
+    end
   end
   
   def show
     @post = Post.find(params[:id])
   
+  end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update_attributes(params[:post])
+      redirect_to :action => :show, :id => @post.id
+    else
+      render 'edit'
+    end
   end
 
 end
