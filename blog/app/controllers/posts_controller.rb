a32f833ac9ca8e6ff20fd5fc18193e76ee5eb33a
eland <mdanskey@gmail.com>
2012-08-07 12:00:15 -0700

    Deleting posts implemented

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -36,5 +36,12 @@ class PostsController < ApplicationController
       render 'edit'
     end
   end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    
+    redirect_to :action => :index
+  end
 
 end
