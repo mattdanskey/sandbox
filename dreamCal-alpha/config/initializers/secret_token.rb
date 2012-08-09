6d7ee7749c6300d71eea47da1d3cb7db10a37c15
eland <mdanskey@gmail.com>
2012-08-08 20:58:47 -0700

    First commit for my first independent app

---
+++ dreamCal-alpha/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamCalAlpha::Application.config.secret_token = '84cd19b585a7ef5aa74d41ba5e51a1da58c253e3a32c22f1d28dc7974d94d29023bd1aca36ed4241eaf657525c09d6798c3a97703e44d0a7e274c665403e3226'
